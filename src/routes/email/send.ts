79f7c170f7c4f34ae3c5fe5b24982e6b1d828335
Abdulaziz Alimov <alimoabd2127@gmail.com>
2020-08-03 19:29:12 -0400

    Fixed subject missing error.

--- src/routes/email/send.ts
+++ src/routes/email/send.ts
@@ -4,19 +4,23 @@ import Joi from 'joi'
 import Router from 'koa-router'
 
 const schema = Joi.object({
-  message: Joi.string().required()
+  message: Joi.string().required(),
+  subject: Joi.string().optional()
 })
 
 export default async (router: Router): Promise<void> => {
   router.post('/send', verifyModelMiddleware(schema), async (ctx) => {
-    sendToEmailSubscribers(ctx.request.body.message)
+    sendToEmailSubscribers(
+      ctx.request.body.subject || 'AWS-Messenger-Notification',
+      ctx.request.body.message
+    )
     ctx.response.status = 200
   })
 }
 
-function sendToEmailSubscribers(message: string) {
+function sendToEmailSubscribers(subject: string, message: string) {
   const source = config.email.source
   const to = config.email.to
   const replyTo = config.email.replyTo as string[]
-  sendEmail(source, to, replyTo, message)
+  sendEmail(source, to, replyTo, subject, message)
 }
